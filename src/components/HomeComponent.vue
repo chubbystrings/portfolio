2aaf6c4518ab079c385c204ee838a51f5ea49f80
chubbystrings <martinsokwor@hotmail.com>
2021-02-14 18:48:42 +0100

    added new project webcoloz

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -308,30 +308,14 @@ export default {
 
   data() {
     return {
-      articles: [
-        {
-          src: 'https://images.unsplash.com/photo-1423784346385-c1d4dac9893a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-          title: 'Mobile first & Responsive',
-          text: 'Phasellus lorem enim, luctus ut velit eget, convallis egestas eros. Sed ornare ligula eget tortor tempor, quis porta tellus dictum.',
-        },
-        {
-          src: 'https://images.unsplash.com/photo-1475938476802-32a7e851dad1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-          title: 'Think outside the box',
-          text: 'Nam ut leo ipsum. Maecenas pretium aliquam feugiat. Aenean vel tempor est, vitae tincidunt risus. Sed sodales vestibulum nibh.',
-        },
-        {
-          src: 'https://images.unsplash.com/photo-1416339442236-8ceb164046f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1892&q=80',
-          title: 'Small changes, big difference',
-          text: 'Vestibulum in dictum velit, in rhoncus nibh. Maecenas neque libero, interdum a dignissim in, aliquet vitae lectus. Phasellus lorem enim, luctus ut velit eget.',
-        },
-      ],
+      articles: [],
       features: [
         {
           icon: 'mdi-account-group-outline',
-          title: 'WEBCOLORS',
-          text: 'Webcolors is a search engine  web app for html colors with over 146 colors available in hex, rgba format ',
-          tech: 'VUE JS',
-          url: 'https://master--webcolors.netlify.com/',
+          title: 'WEBCOLORZ',
+          text: 'Webcolors is a search engine  app for html colors with over 146 colors available in hex, rgb and name format ',
+          tech: 'VUE JS && NODE JS',
+          url: 'https://webcoloz.web.app',
         },
         {
           icon: 'mdi-update',
@@ -384,13 +368,6 @@ export default {
           text: 'Teamplay is a social web app built for the workplace, Users can create article posts, gif posts, comment, like posts etc',
           tech: 'NODE JS AND VUE JS',
           url: 'https://teamplay-app-e48a7.firebaseapp.com/',
-        },
-        {
-          icon: 'mdi-shield-outline',
-          title: 'FITAZ',
-          text: 'Fitaz app is a social web app for fitness enthusiasts, where users in same location can connect with each other for fitness activities',
-          tech: 'PHP OOP',
-          url: 'http://emekaokwor.com/fitaz',
         },
 
         {
